ed8c47c68a6b0ae32114eefc27c5bd96e30f4544
Joshua Kelley <loredevelops@gmail.com>
2020-06-26 10:43:28 -0600

    Adding MongoDB and some NPM packages.

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 // Initiliazing express.js
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose")
+const dotenv = require("dotenv");
 
+// Initilize packages
 const app = express();
+dotenv.config();
+
+// DB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log('DB Connected'));
+mongoose.connection.on('error', err =>{
+    console.log(`DB Connection Error: ${err.message}`);
+});
 
 // Bring in routes
 const postRoutes = require('./routes/post');
 
 // Middleware
 app.use(morgan("dev"));
-
-// Homepage Route
 app.use('/', postRoutes);
 
 // Listening to port
-const port = 8080;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log(`A Node.js API is litening on port ${port}`)
 });
